75bf96aa96dab2bb30310a8ec23cc456c3c56e3b
ecastillo <ecastillo2020@gmail.com>
2017-04-06 16:51:50 -0700

    SurviveJS - CH3 Finished

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,83 +1,49 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import merge from 'webpack-merge';
+
+import {lintJavaScript, devServer} from './webpack.parts';
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-const commonConfig = {
+const commonConfig = merge([
+  {
 
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-
-        loader: 'eslint-loader',
-        options: {
-          emitWarning: true,
-        },
-      },
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
+      }),
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo',
-    }),
-  ],
-};
+  lintJavaScript({include: PATHS.app}),
 
+]);
 
-const productionConfig = () => commonConfig;
+const productionConfig = merge([]);
 
-const developmentConfig = () => {
-  const config = {
-    devServer: {
-      overlay: {
-        errors: true,
-        warnings: true,
-      },
-
-      // Enable history API fallback so HTML5 History API based
-      // routing works. Good for complex setups.
-      historyApiFallback: true,
-
-      // Display only errors to reduce the amount of output.
-      stats: 'errors-only',
-
-      // Parse host and port from env to allow customization.
-      //
-      // If you use Docker, Vagrant or Cloud9, set
-      // host: options.host || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices
-      // unlike default `localhost`.
-      host: process.env.HOST, // Defaults to `localhost`
-      port: process.env.PORT, // Defaults to 8080
-    },
-  };
-
-  return Object.assign(
-    {},
-    commonConfig,
-    config
-  );
-};
-
+const developmentConfig = merge([
+  devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
 
 export default (env) => {
   if (env === 'production') {
-    return productionConfig();
+    return merge(commonConfig, productionConfig);
   }
 
-  return developmentConfig();
+  return merge(commonConfig, developmentConfig);
 
 };
